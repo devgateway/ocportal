94f84918ae0eefed32e45b8c6b14af7eb250dac1
Ionut Dobre <idobre@developmentgateway.org>
2019-04-17 13:55:35 +0300

    OCMAKU-43 - added unique label constraint.

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/category/EditTargetGroupPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/category/EditTargetGroupPage.java
@@ -3,6 +3,9 @@ package org.devgateway.toolkit.forms.wicket.page.edit.category;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.devgateway.toolkit.forms.validators.UniquePropertyEntryValidator;
+import org.devgateway.toolkit.persistence.dao.categories.ChargeAccount_;
+import org.devgateway.toolkit.persistence.dao.categories.TargetGroup_;
 import org.devgateway.toolkit.web.security.SecurityConstants;
 import org.devgateway.toolkit.forms.wicket.page.lists.category.ListTargetGroupPage;
 import org.devgateway.toolkit.persistence.dao.categories.TargetGroup;
@@ -25,8 +28,18 @@ public class EditTargetGroupPage extends AbstractCategoryEditPage<TargetGroup> {
         listPageClass = ListTargetGroupPage.class;
     }
 
+    private void addUniqueNameValidator() {
+        label.getField().add(new UniquePropertyEntryValidator<>(
+                getString("uniqueName"),
+                targetGroupService::findOne,
+                (o, v) -> (root, query, cb) -> cb.equal(root.get(TargetGroup_.label), v),
+                editForm.getModel()
+        ));
+    }
+
     @Override
     protected void onInitialize() {
         super.onInitialize();
+        addUniqueNameValidator();
     }
 }
