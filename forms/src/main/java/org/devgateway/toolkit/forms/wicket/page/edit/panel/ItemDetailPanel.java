985b4339ed93bcb5970142041f6ff711f67b897c
Ionut Dobre <idobre@developmentgateway.org>
2019-04-18 16:46:15 +0300

    OCMAKU-10 - added plan item fields.

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/panel/ItemDetailPanel.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/panel/ItemDetailPanel.java
@@ -1,16 +1,25 @@
 package org.devgateway.toolkit.forms.wicket.page.edit.panel;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.devgateway.toolkit.forms.WebConstants;
 import org.devgateway.toolkit.forms.wicket.components.ListViewSectionPanel;
+import org.devgateway.toolkit.forms.wicket.components.StopEventPropagationBehavior;
+import org.devgateway.toolkit.forms.wicket.components.util.ComponentUtil;
 import org.devgateway.toolkit.persistence.dao.form.ItemDetail;
 import org.devgateway.toolkit.persistence.dao.form.PurchaseRequisition;
+import org.devgateway.toolkit.persistence.service.form.PlanItemService;
 
 /**
  * @author idobre
  * @since 2019-04-17
  */
 public class ItemDetailPanel extends ListViewSectionPanel<ItemDetail, PurchaseRequisition> {
+    @SpringBean
+    private PlanItemService planItemService;
+
     public ItemDetailPanel(final String id) {
         super(id);
     }
@@ -32,7 +41,13 @@ public class ItemDetailPanel extends ListViewSectionPanel<ItemDetail, PurchaseRe
 
     @Override
     public void populateCompoundListItem(final ListItem<ItemDetail> item) {
+        final Component planItem = ComponentUtil.addSelect2ChoiceField(item, "planItem", planItemService).required();
+        planItem.add(new StopEventPropagationBehavior());
 
+        ComponentUtil.addIntegerTextField(item, "quantity").required();
+        ComponentUtil.addTextField(item, "unit").required()
+                .getField().add(WebConstants.StringValidators.MAXIMUM_LENGTH_VALIDATOR_STD_DEFAULT_TEXT);
+        ComponentUtil.addDoubleField(item, "amount");
     }
 
     @Override
