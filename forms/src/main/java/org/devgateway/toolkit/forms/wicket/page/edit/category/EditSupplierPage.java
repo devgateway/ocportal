3f25d37d21a8a731426cd0fdcec11dac2b50e1b3
Gerald Mutuhu <geraldmutuhu@gmail.com>
2019-04-20 00:24:17 +0300

    OCMAKU-40 Fix error on save, caused by long  text in address field
    OCMAKU-40 Fix error on save, caused by long  text in address field

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/category/EditSupplierPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/category/EditSupplierPage.java
@@ -6,6 +6,8 @@ package org.devgateway.toolkit.forms.wicket.page.edit.category;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.devgateway.toolkit.forms.WebConstants;
+import org.devgateway.toolkit.forms.wicket.components.form.TextAreaFieldBootstrapFormComponent;
 import org.devgateway.toolkit.forms.wicket.components.util.ComponentUtil;
 import org.devgateway.toolkit.forms.wicket.page.lists.category.ListSupplierPage;
 import org.devgateway.toolkit.persistence.dao.categories.Supplier;
@@ -34,6 +36,8 @@ public class EditSupplierPage extends AbstractCategoryEditPage<Supplier> {
         super.onInitialize();
 
         addCode();
-        ComponentUtil.addTextAreaField(editForm, "address");
+        TextAreaFieldBootstrapFormComponent<String> address = ComponentUtil.addTextAreaField(editForm, "address");        
+        address.getField().add(WebConstants.StringValidators.MAXIMUM_LENGTH_VALIDATOR_STD_DEFAULT_TEXT);  
+        
     }
 }
