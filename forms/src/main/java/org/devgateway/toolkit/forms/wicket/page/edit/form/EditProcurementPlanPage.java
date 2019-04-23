2ae13921a4b1d4a1aff42b89c02a41d47b66f8df
Ionut Dobre <idobre@developmentgateway.org>
2019-04-23 14:17:21 +0300

    OCMAKU-26 - Non-Admins users should have the Department preselected when adding a new Procurement Plan

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditProcurementPlanPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditProcurementPlanPage.java
@@ -9,16 +9,18 @@ import org.apache.wicket.validation.IValidator;
 import org.apache.wicket.validation.ValidationError;
 import org.devgateway.toolkit.forms.WebConstants;
 import org.devgateway.toolkit.forms.wicket.components.form.FileInputBootstrapFormComponent;
-import org.devgateway.toolkit.forms.wicket.components.form.Select2ChoiceBootstrapFormComponent;
+import org.devgateway.toolkit.forms.wicket.components.form.GenericSleepFormComponent;
 import org.devgateway.toolkit.forms.wicket.components.util.ComponentUtil;
 import org.devgateway.toolkit.forms.wicket.page.edit.panel.PlanItemPanel;
 import org.devgateway.toolkit.forms.wicket.page.lists.form.ListProcurementPlanPage;
+import org.devgateway.toolkit.persistence.dao.Person;
 import org.devgateway.toolkit.persistence.dao.categories.Department;
 import org.devgateway.toolkit.persistence.dao.categories.FiscalYear;
 import org.devgateway.toolkit.persistence.dao.form.ProcurementPlan;
 import org.devgateway.toolkit.persistence.service.category.DepartmentService;
 import org.devgateway.toolkit.persistence.service.category.FiscalYearService;
 import org.devgateway.toolkit.persistence.service.form.ProcurementPlanService;
+import org.devgateway.toolkit.web.WebSecurityUtil;
 import org.devgateway.toolkit.web.security.SecurityConstants;
 import org.wicketstuff.annotation.mount.MountPath;
 
@@ -38,8 +40,6 @@ public class EditProcurementPlanPage extends EditAbstractMakueniFormPage<Procure
     @SpringBean
     private FiscalYearService fiscalYearService;
 
-    private Select2ChoiceBootstrapFormComponent<FiscalYear> fiscalYear;
-
     public EditProcurementPlanPage(final PageParameters parameters) {
         super(parameters);
 
@@ -51,10 +51,16 @@ public class EditProcurementPlanPage extends EditAbstractMakueniFormPage<Procure
     protected void onInitialize() {
         super.onInitialize();
 
-        ComponentUtil.addSelect2ChoiceField(editForm, "department", departmentService).required()
+        // Non-Admins users should have the Department preselected when adding a new Procurement Plan
+        if (WebSecurityUtil.isCurrentUserAdmin()) {
+            ComponentUtil.addSelect2ChoiceField(editForm, "department", departmentService).required();
+        } else {
+            editForm.add(new GenericSleepFormComponent<>("department"));
+        }
+
+        ComponentUtil.addSelect2ChoiceField(editForm, "fiscalYear", fiscalYearService)
+                .required()
                 .getField().add(uniqueProcurementPlan());
-        fiscalYear = ComponentUtil.addSelect2ChoiceField(editForm, "fiscalYear", fiscalYearService);
-        fiscalYear.required();
 
         editForm.add(new PlanItemPanel("planItems"));
 
@@ -66,12 +72,25 @@ public class EditProcurementPlanPage extends EditAbstractMakueniFormPage<Procure
         ComponentUtil.addDateField(editForm, "approvedDate").required();
     }
 
-    private IValidator<Department> uniqueProcurementPlan() {
+    @Override
+    protected ProcurementPlan newInstance() {
+        final ProcurementPlan procurementPlan = super.newInstance();
+
+        // non-admin users should create a Procurement Plan only with their department
+        if (!WebSecurityUtil.isCurrentUserAdmin()) {
+            final Person person = WebSecurityUtil.getCurrentAuthenticatedPerson();
+            procurementPlan.setDepartment(person.getDepartment());
+        }
+
+        return procurementPlan;
+    }
+
+    private IValidator<FiscalYear> uniqueProcurementPlan() {
         StringValue id = getPageParameters().get(WebConstants.PARAM_ID);
         return new UniqueProcurementPlanValidator(id.toLong(-1));
     }
 
-    public class UniqueProcurementPlanValidator implements IValidator<Department> {
+    public class UniqueProcurementPlanValidator implements IValidator<FiscalYear> {
         private final Long id;
 
         public UniqueProcurementPlanValidator(final Long id) {
@@ -79,13 +98,13 @@ public class EditProcurementPlanPage extends EditAbstractMakueniFormPage<Procure
         }
 
         @Override
-        public void validate(final IValidatable<Department> validatable) {
-            final Department department = validatable.getValue();
-            final FiscalYear fiscalYearValue = fiscalYear.getModelObject();
+        public void validate(final IValidatable<FiscalYear> validatable) {
+            final FiscalYear fiscalYear = validatable.getValue();
+            final Department department = editForm.getModelObject().getDepartment();
 
-            if (department != null && fiscalYearValue != null) {
+            if (department != null && fiscalYear != null) {
                 if (procurementPlanService
-                        .countByDepartmentAndFiscalYearAndIdNot(department, fiscalYearValue, id) > 0) {
+                        .countByDepartmentAndFiscalYearAndIdNot(department, fiscalYear, id) > 0) {
                     final ValidationError error = new ValidationError(getString("uniqueProcurementPlan"));
                     validatable.error(error);
                 }
