386a572f8d9358dc565ebcf875aa4ec96df5cfe8
Gerald Mutuhu <geraldmutuhu@gmail.com>
2019-04-26 14:35:07 +0300

    OCMAKU-39 Staff title causes error when input is very long
    OCMAKU-39 Staff title causes error when input is very long

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/category/EditStaffPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/category/EditStaffPage.java
@@ -6,6 +6,8 @@ package org.devgateway.toolkit.forms.wicket.page.edit.category;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.devgateway.toolkit.forms.WebConstants;
+import org.devgateway.toolkit.forms.wicket.components.form.TextFieldBootstrapFormComponent;
 import org.devgateway.toolkit.forms.wicket.components.util.ComponentUtil;
 import org.devgateway.toolkit.forms.wicket.page.lists.category.ListStaffPage;
 import org.devgateway.toolkit.persistence.dao.categories.Staff;
@@ -37,7 +39,9 @@ public class EditStaffPage extends AbstractCategoryEditPage<Staff> {
     protected void onInitialize() {
         super.onInitialize();
 
-        ComponentUtil.addTextField(editForm, "title");
+        final TextFieldBootstrapFormComponent<String> title = ComponentUtil.addTextField(editForm, "title");
+        title.getField().add(WebConstants.StringValidators.MAXIMUM_LENGTH_VALIDATOR_STD_DEFAULT_TEXT);
+        
         ComponentUtil.addSelect2ChoiceField(editForm, "department", departmentService).required();
     }
 }
