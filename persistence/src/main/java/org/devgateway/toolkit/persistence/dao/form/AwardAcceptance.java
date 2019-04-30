8fbfafbea54ba2f72212bf90a6ec47a9b8813b45
Gerald Mutuhu <geraldmutuhu@gmail.com>
2019-04-30 10:33:08 +0300

    OCMAKU-47 Acceptance of Award Form
    OCMAKU-47 Acceptance of Award Form

---
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/dao/form/AwardAcceptance.java
@@ -0,0 +1,66 @@
+package org.devgateway.toolkit.persistence.dao.form;
+
+import java.util.Date;
+import org.devgateway.toolkit.persistence.dao.categories.Supplier;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Index;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.envers.Audited;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * @author gmutuhu
+ *
+ */
+@Entity
+@Audited
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Table(indexes = {@Index(columnList = "procurement_plan_id")})
+public class AwardAcceptance extends AbstractMakueniForm {
+    @OneToOne(fetch = FetchType.LAZY)
+    @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+    @JsonIgnore    
+    private TenderQuotationEvaluation tenderQuotationEvaluation; 
+    private Date acceptanceDate;
+    private Supplier awardee;
+    
+
+    public TenderQuotationEvaluation getTenderQuotationEvaluation() {
+        return tenderQuotationEvaluation;
+    }
+
+    public void setTenderQuotationEvaluation(final TenderQuotationEvaluation tenderQuotationEvaluation) {
+        this.tenderQuotationEvaluation = tenderQuotationEvaluation;
+    }
+
+    public Date getAcceptanceDate() {
+        return acceptanceDate;
+    }
+
+    public void setAcceptanceDate(final Date acceptanceDate) {
+        this.acceptanceDate = acceptanceDate;
+    }
+
+    public Supplier getAwardee() {
+        return awardee;
+    }
+
+    public void setAwardee(final Supplier awardee) {
+        this.awardee = awardee;
+    }
+
+    @Override
+    public void setLabel(final String label) {        
+    }
+
+    @Override
+    public String getLabel() {
+       return null;
+    }
+
+}
