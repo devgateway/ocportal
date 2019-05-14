3c9fe08bc2c225930d92bab279d0034a20aa9777
Ionut Dobre <idobre@developmentgateway.org>
2019-05-14 11:21:36 +0300

    OCMAKU-33 - subclass dashboard from basepage.

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/overview/DataEntryBasePage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/overview/DataEntryBasePage.java
@@ -11,19 +11,10 @@
  *******************************************************************************/
 package org.devgateway.toolkit.forms.wicket.page.overview;
 
-import de.agilecoders.wicket.core.markup.html.bootstrap.html.HtmlTag;
-import de.agilecoders.wicket.core.markup.html.references.RespondJavaScriptReference;
-import de.agilecoders.wicket.core.markup.html.themes.bootstrap.BootstrapCssReference;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesomeCssReference;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.head.filter.HeaderResponseContainer;
-import org.apache.wicket.markup.html.GenericWebPage;
-import org.apache.wicket.markup.html.TransparentWebMarkupContainer;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.resource.JQueryResourceReference;
-import org.devgateway.toolkit.forms.wicket.styles.BaseStyles;
+import org.devgateway.toolkit.forms.wicket.page.BasePage;
 import org.devgateway.toolkit.forms.wicket.styles.OverviewStyles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,21 +22,21 @@ import org.slf4j.LoggerFactory;
 /**
  * TODO - check if this can be merged with BasePage.
  */
-public abstract class DataEntryBasePage extends GenericWebPage<Void> {
+public abstract class DataEntryBasePage extends BasePage {
     protected static final Logger logger = LoggerFactory.getLogger(DataEntryBasePage.class);
-
-    private final TransparentWebMarkupContainer mainContainer;
 
     public DataEntryBasePage(final PageParameters parameters) {
         super(parameters);
+    }
 
-        add(new HtmlTag("html"));
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+    }
 
-        // Add javascript files.
-        add(new HeaderResponseContainer("scripts-container", "scripts-bucket"));
-
-        mainContainer = new TransparentWebMarkupContainer("mainContainer");
-        add(mainContainer);
+    @Override
+    public Boolean fluidContainer() {
+        return true;
     }
 
     @Override
@@ -53,13 +44,6 @@ public abstract class DataEntryBasePage extends GenericWebPage<Void> {
         super.renderHead(response);
 
         // Load Styles.
-        response.render(CssHeaderItem.forReference(BaseStyles.INSTANCE));
-        response.render(CssHeaderItem.forReference(BootstrapCssReference.instance()));
-        response.render(CssHeaderItem.forReference(FontAwesomeCssReference.instance()));
         response.render(CssHeaderItem.forReference(OverviewStyles.INSTANCE));
-
-        // Load Scripts.
-        response.render(RespondJavaScriptReference.headerItem());
-        response.render(JavaScriptHeaderItem.forReference(JQueryResourceReference.getV2()));
     }
 }
