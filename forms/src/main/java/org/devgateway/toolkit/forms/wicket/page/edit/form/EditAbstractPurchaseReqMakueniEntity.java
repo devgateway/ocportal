43edc660d710281a0d5b4a9ec3114027cf6e8672
Mihai Postelnicu <mpostelnicu@gmail.com>
2019-05-24 16:36:50 +0300

    OCMAKU-54 Cancel (terminated) button implementation

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditAbstractPurchaseReqMakueniEntity.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditAbstractPurchaseReqMakueniEntity.java
@@ -1,7 +1,9 @@
 package org.devgateway.toolkit.forms.wicket.page.edit.form;
 
+import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.devgateway.toolkit.forms.wicket.components.util.SessionUtil;
+import org.devgateway.toolkit.forms.wicket.events.EditingDisabledEvent;
 import org.devgateway.toolkit.forms.wicket.page.overview.status.StatusOverviewPage;
 import org.devgateway.toolkit.persistence.dao.form.AbstractPurchaseReqMakueniEntity;
 import org.devgateway.toolkit.persistence.dao.form.PurchaseRequisition;
@@ -16,6 +18,19 @@ public abstract class EditAbstractPurchaseReqMakueniEntity<T extends AbstractPur
     protected static final Logger logger = LoggerFactory.getLogger(EditAbstractPurchaseReqMakueniEntity.class);
 
     protected final PurchaseRequisition purchaseRequisition;
+
+    @Override
+    protected void checkAndSendEventForDisableEditing() {
+        super.checkAndSendEventForDisableEditing();
+        if (isTerminated()) {
+            send(getPage(), Broadcast.BREADTH, new EditingDisabledEvent());
+        }
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return super.isTerminated() || (purchaseRequisition != null && purchaseRequisition.isTerminated());
+    }
 
     public EditAbstractPurchaseReqMakueniEntity(final PageParameters parameters) {
         super(parameters);
