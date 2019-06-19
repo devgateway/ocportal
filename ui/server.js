07579ea749f860c85b0923754e84183830e79186
Ionut Dobre <idobre@developmentgateway.org>
2019-06-19 11:02:37 +0300

    update conf for development.

--- ui/server.js
+++ ui/server.js
@@ -2,11 +2,25 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.dev.config');
 
-new WebpackDevServer(webpack(config), {
+// Change URL to the backend API here!
+
+var server = new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', (err, result) => {
+  historyApiFallback: true,
+  proxy: {
+    "/api": {
+      "target": {
+        "host": "localhost",
+        "protocol": 'http:',
+        "port": 8090
+      },
+      secure: false
+    }
+  }
+});
+
+server.listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
