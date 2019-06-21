88459a83f1b8badbda06ae9c686b4aaf8f35af6b
Ionut Dobre <idobre@developmentgateway.org>
2019-06-21 14:53:40 +0300

    OCMAKU-147 - update filters location.

--- ui/oce-standalone/index.jsx
+++ ui/oce-standalone/index.jsx
@@ -20,6 +20,7 @@ class OCEDemoLocation extends LocationTab {
     return window.innerHeight - TOP_OFFSET - BOTTOM_OFFSET;
   }
 }
+
 OCEDemoLocation.CENTER = [37, -100];
 
 class OCEMakueni extends OCApp {
@@ -31,16 +32,17 @@ class OCEMakueni extends OCApp {
     this.registerTab(EfficiencyTab);
     this.registerTab(EProcurementTab);
   }
-
+  
   fetchBidTypes() {
-    fetchJson('/api/ocds/bidType/all').then(data =>
+    fetchJson('/api/ocds/bidType/all')
+    .then(data =>
       this.setState({
         bidTypes: data.reduce((map, datum) =>
           map.set(datum.id, datum.description), Map())
       })
     );
   }
-
+  
   loginBox() {
     let linkUrl;
     let text;
@@ -59,7 +61,7 @@ class OCEMakueni extends OCApp {
       </a>
     );
   }
-
+  
   dashboardSwitcher() {
     const { dashboardSwitcherOpen } = this.state;
     const { onSwitch } = this.props;
@@ -67,25 +69,26 @@ class OCEMakueni extends OCApp {
       <div className={cn('dash-switcher-wrapper', { open: dashboardSwitcherOpen })}>
         <h1 onClick={this.toggleDashboardSwitcher.bind(this)}>
           <strong>Monitoring & Evaluation</strong> Toolkit
-          <i className="glyphicon glyphicon-menu-down" />
+          <i className="glyphicon glyphicon-menu-down"/>
         </h1>
         {dashboardSwitcherOpen &&
-          <div className="dashboard-switcher">
-            <a href="javascript:void(0);" onClick={() => onSwitch('crd')}>
-              Corruption Risk Dashboard
-            </a>
-          </div>
+        <div className="dashboard-switcher">
+          <a href="javascript:void(0);" onClick={() => onSwitch('crd')}>
+            Corruption Risk Dashboard
+          </a>
+        </div>
         }
       </div>
     );
   }
-
+  
   exportBtn() {
-    if(this.state.exporting) {
+    if (this.state.exporting) {
       return (
         <div className="export-progress">
           <div className="progress">
-            <div className="progress-bar progress-bar-danger" role="progressbar" style={{ width: "100%" }}>
+            <div className="progress-bar progress-bar-danger" role="progressbar"
+                 style={{ width: '100%' }}>
               {this.t('export:exporting')}
             </div>
           </div>
@@ -95,41 +98,43 @@ class OCEMakueni extends OCApp {
     return (
       <div className="export-btn">
         <button className="btn btn-default" disabled>
-          <i className="glyphicon glyphicon-download-alt" />
+          <i className="glyphicon glyphicon-download-alt"/>
         </button>
       </div>
     );
   }
-
+  
   languageSwitcher() {
     const { TRANSLATIONS } = this.constructor;
     const { locale: selectedLocale } = this.state;
     if (Object.keys(TRANSLATIONS).length <= 1) return null;
-    return Object.keys(TRANSLATIONS).map(locale => (
+    return Object.keys(TRANSLATIONS)
+    .map(locale => (
       <a key={locale}
-        href="javascript:void(0);"
-        onClick={() => this.setLocale(locale)}
-        className={cn({ active: locale === selectedLocale })}
+         href="javascript:void(0);"
+         onClick={() => this.setLocale(locale)}
+         className={cn({ active: locale === selectedLocale })}
       >
         {locale.split('_')[0]}
       </a>
     ));
   }
-
-  render(){
+  
+  render() {
     return (
-      <div className="container-fluid dashboard-default" onClick={() => this.setState({ menuBox: '' })}>
+      <div className="container-fluid dashboard-default"
+           onClick={() => this.setState({ menuBox: '' })}>
         <header className="branding row">
           <div className="col-sm-3">
             <div className="logo-wrapper">
               <img src="assets/makueni-logo.png" alt="Makueni"/>
             </div>
-  
+            
             <div className="header-icons language-switcher">
               {this.languageSwitcher()}
             </div>
           </div>
-  
+          
           <div className="col-sm-7">
             <div className="row">
               <div className="navigation">
@@ -147,29 +152,32 @@ class OCEMakueni extends OCApp {
             {this.dashboardSwitcher()}
           </div>
           
-          <div className="col-md-5 menu">
-            <div className="filters-hint">
-              {this.t('filters:hint')}
-            </div>
-            {this.filters()}
-            {this.comparison()}
+          <div className="col-md-5">
+          
           </div>
           <div className="col-md-4">
             {this.exportBtn()}
           </div>
         </div>
-  
-        <div className="col-xs-4 col-md-3">
-          <div className="row">
-            {/* TODO filters*/}
+        
+        <div className="row">
+          <div className="col-xs-4 col-md-3 menu">
+            <div className="row">
+              <div className="filters-hint col-md-12">
+                {this.t('filters:hint')}
+              </div>
+              {this.filters()}
+              {this.comparison()}
+            </div>
+          </div>
+          
+          <div className="col-xs-8 col-md-9">
+            <div className="row">
+              {this.content()}
+            </div>
           </div>
         </div>
         
-        <div className="col-xs-offset-4 col-md-offset-3 col-xs-8 col-md-9">
-          <div className="row">
-            {this.content()}
-          </div>
-        </div>
         {this.showMonths() && <div
           className="col-xs-offset-4 col-md-offset-3 col-xs-8 col-md-9 months-bar"
           role="navigation"
@@ -197,7 +205,7 @@ const translations = {
 const BILLION = 1000000000;
 const MILLION = 1000000;
 const THOUSAND = 1000;
-const formatNumber = number => number.toLocaleString(undefined, {maximumFractionDigits: 2});
+const formatNumber = number => number.toLocaleString(undefined, { maximumFractionDigits: 2 });
 
 const styling = {
   charts: {
@@ -225,7 +233,8 @@ CorruptionRickDashboard.STYLING = JSON.parse(JSON.stringify(styling));
 
 CorruptionRickDashboard.STYLING.charts.traceColors = ['#234e6d', '#3f7499', '#80b1d3', '#afd5ee', '#d9effd'];
 
-class OceSwitcher extends ViewSwitcher {}
+class OceSwitcher extends ViewSwitcher {
+}
 
 OceSwitcher.views['m-and-e'] = OCEMakueni;
 OceSwitcher.views.crd = CorruptionRickDashboard;
--- ui/oce/filters/index.jsx
+++ ui/oce/filters/index.jsx
@@ -1,94 +1,98 @@
-import Component from "../pure-render-component";
-import translatable from "../translatable";
-import cn from "classnames";
-import Organizations from "./tabs/organizations";
-import ProcurementRules from "./tabs/procurement-rules.jsx";
-import Amounts from "./tabs/amounts";
-import {Map} from "immutable";
-import {send, fetchJson, callFunc} from "../tools";
-import URI from "urijs";
-import {DropdownButton, MenuItem} from "react-bootstrap";
-import {fromJSON, toJSON} from "transit-immutable-js";
+import Component from '../pure-render-component';
+import translatable from '../translatable';
+import cn from 'classnames';
+import Organizations from './tabs/organizations';
+import ProcurementRules from './tabs/procurement-rules.jsx';
+import Amounts from './tabs/amounts';
+import { Map } from 'immutable';
+import { send, fetchJson, callFunc } from '../tools';
+import URI from 'urijs';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { fromJSON, toJSON } from 'transit-immutable-js';
 
 const dashboardId = new URI(location).search(true).dashboardId;
 
-class Filters extends translatable(Component){
-  constructor(props){
+class Filters extends translatable(Component) {
+  constructor(props) {
     super(props);
     this.state = {
       currentTab: 0,
       state: Map(),
       savingDashboard: false,
-      dashboardName: "",
+      dashboardName: '',
       dashboards: []
     };
-
+    
     const save = ep => () => {
-      const {state, dashboardName} = this.state;
+      const { state, dashboardName } = this.state;
       const encoded = toJSON(state);
       send(new URI(`/rest/userDashboards/${ep}`)
-          .addSearch('name', dashboardName)
-          .addSearch('formUrlEncodedBody', encoded)
-      ).then(callFunc('text')).then(response => response.length ?
-          alert(this.t('filters:dashboard:saveError')) : //response text means error
-          this.fetchDashboards()//no response text means success, on success, no response is expected
+        .addSearch('name', dashboardName)
+        .addSearch('formUrlEncodedBody', encoded)
+      )
+      .then(callFunc('text'))
+      .then(response => response.length ?
+        alert(this.t('filters:dashboard:saveError')) : //response text means error
+        this.fetchDashboards()//no response text means success, on success, no response is expected
       );
       this.setState({
         savingDashboard: false,
-        dashboardName: ""
-      })
+        dashboardName: ''
+      });
     };
-
-    this.saveForCurrentUser = save('saveDashboardForCurrentUser')
+    
+    this.saveForCurrentUser = save('saveDashboardForCurrentUser');
     this.saveUnassigned = save('saveDashboard');
   }
-
-  listTabs(){
-    let {currentTab} = this.state;
+  
+  listTabs() {
+    let { currentTab } = this.state;
     return this.constructor.TABS.map((Tab, index) => <li
-            key={index}
-            role="presentation"
-            className={cn({active: index == currentTab})}
-            onClick={_ => this.setState({currentTab: index})}
-        >
-          <a href="javascript:void(0);">
-            {Tab.getName(this.t.bind(this))}
-          </a>
-        </li>
+        key={index}
+        role="presentation"
+        className={cn({ active: index == currentTab })}
+        onClick={_ => this.setState({ currentTab: index })}
+      >
+        <a href="javascript:void(0);">
+          {Tab.getName(this.t.bind(this))}
+        </a>
+      </li>
     );
   }
-
-  content(){
-    let {currentTab, state} = this.state;
-    let {bidTypes, translations} = this.props;
+  
+  content() {
+    let { currentTab, state } = this.state;
+    let { bidTypes, translations } = this.props;
     let Component = this.constructor.TABS[currentTab];
     return <Component
-        state={state}
-        onUpdate={(key, update) => this.setState({state: this.state.state.set(key, update)})}
-        bidTypes={bidTypes}
-        translations={translations}
-    />
+      state={state}
+      onUpdate={(key, update) => this.setState({ state: this.state.state.set(key, update) })}
+      bidTypes={bidTypes}
+      translations={translations}
+    />;
   }
-
-  reset(){
-    this.setState({state: Map()});
-    this.props.onUpdate(Map())
+  
+  reset() {
+    this.setState({ state: Map() });
+    this.props.onUpdate(Map());
   }
-
-  componentDidMount(){
+  
+  componentDidMount() {
   
   }
-
-  updateFilters(newFilters){
-    this.setState({state: newFilters});
+  
+  updateFilters(newFilters) {
+    this.setState({ state: newFilters });
     this.props.onUpdate(newFilters);
   }
-
-  render(){
-    let {onClick, onUpdate, open, user} = this.props;
-    const {savingDashboard, dashboardName, dashboards} = this.state;
-    return <div className={cn('filters', {open})}  onClick={onClick}>
-      <img className="top-nav-icon" src="assets/icons/filter.svg" width="100%" height="100%"/> {this.t('filters:title')} <i className="glyphicon glyphicon-menu-down"></i>
+  
+  render() {
+    let { onClick, onUpdate, open, user } = this.props;
+    const { savingDashboard, dashboardName, dashboards } = this.state;
+    return <div className={cn('filters', { open })} onClick={onClick}>
+      <img className="top-nav-icon" src="assets/icons/filter.svg" width="100%"
+           height="100%"/> {this.t('filters:title')} <i
+      className="glyphicon glyphicon-menu-down"></i>
       <div className="box row" onClick={e => e.stopPropagation()}>
         <ul className="nav nav-pills nav-stacked col-xs-4">
           {this.listTabs()}
@@ -106,46 +110,50 @@ class Filters extends translatable(Component){
           </button>
           &nbsp;
           {user.loggedIn && !savingDashboard &&
-            <button className="btn btn-default" onClick={e => this.setState({savingDashboard: true})}>
-              {this.t('filters:dashboard:save')}
-            </button>
+          <button className="btn btn-default"
+                  onClick={e => this.setState({ savingDashboard: true })}>
+            {this.t('filters:dashboard:save')}
+          </button>
           }
           {savingDashboard &&
-            <input
-                type="text"
-                className="input-sm form-control dashboard-name"
-                value={dashboardName}
-                onChange={e => this.setState({dashboardName: e.target.value})}
-            />
+          <input
+            type="text"
+            className="input-sm form-control dashboard-name"
+            value={dashboardName}
+            onChange={e => this.setState({ dashboardName: e.target.value })}
+          />
           }
           &nbsp;
           {savingDashboard && !user.isAdmin &&
-              <button className="btn btn-default" onClick={e => this.saveForCurrentUser()}>
-                {this.t('filters:dashboard:save')}
-              </button>
+          <button className="btn btn-default" onClick={e => this.saveForCurrentUser()}>
+            {this.t('filters:dashboard:save')}
+          </button>
           }
           {savingDashboard && user.isAdmin &&
-              <DropdownButton dropup id="admin-dashboard-save" title={this.t('filters:dashboard:save')}>
-                <MenuItem onClick={e => this.saveForCurrentUser()}>{this.t('filters:dashboard:saveForAdmin')}</MenuItem>
-                <MenuItem onClick={e => this.saveUnassigned()}>{this.t('filters:dashboard:saveUnassigned')}</MenuItem>
-              </DropdownButton>
+          <DropdownButton dropup id="admin-dashboard-save" title={this.t('filters:dashboard:save')}>
+            <MenuItem
+              onClick={e => this.saveForCurrentUser()}>{this.t('filters:dashboard:saveForAdmin')}</MenuItem>
+            <MenuItem
+              onClick={e => this.saveUnassigned()}>{this.t('filters:dashboard:saveUnassigned')}</MenuItem>
+          </DropdownButton>
           }
           {user.loggedIn && !!dashboards.length &&
-              <DropdownButton dropup id="dashboard-load-dropdown" title={this.t('filters:dashboard:load')}>
-                {dashboards.map(({name, formUrlEncodedBody}, index) =>
-                    <MenuItem key={index} onClick={e => this.updateFilters(fromJSON(formUrlEncodedBody))}>
-                      {name}
-                    </MenuItem>
-                )}
-              </DropdownButton>
+          <DropdownButton dropup id="dashboard-load-dropdown"
+                          title={this.t('filters:dashboard:load')}>
+            {dashboards.map(({ name, formUrlEncodedBody }, index) =>
+              <MenuItem key={index} onClick={e => this.updateFilters(fromJSON(formUrlEncodedBody))}>
+                {name}
+              </MenuItem>
+            )}
+          </DropdownButton>
           }
-
+          
           {user.loggedIn && !user.isAdmin && <p className="dashboard-hint">
             {this.t('filters:dashboard:defaultHint')}
             &nbsp;
             <a href={`/account?id=${user.id}`}>{this.t('filters:dashboard:profileSettings')}</a>
           </p>}
-
+          
           {user.loggedIn && user.isAdmin && <p className="dashboard-hint">
             <a href="/listAllDashboards">{this.t('filters:dashboard:manageDashboards')}</a>
             &nbsp;|&nbsp;
@@ -153,7 +161,7 @@ class Filters extends translatable(Component){
           </p>}
         </section>
       </div>
-    </div>
+    </div>;
   }
 }
 
