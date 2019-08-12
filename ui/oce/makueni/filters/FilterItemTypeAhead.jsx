3da9d1660bd7c513949c5263dcabf3e17f903d10
Ionut Dobre <idobre@developmentgateway.org>
2019-08-12 12:04:22 +0300

    control typeahead selection.

--- ui/oce/makueni/filters/FilterItemTypeAhead.jsx
+++ ui/oce/makueni/filters/FilterItemTypeAhead.jsx
@@ -2,6 +2,14 @@ import { Typeahead } from 'react-bootstrap-typeahead';
 import FilterItemSingleSelect from './FilterItemSingleSelect';
 
 class FilterItemTypeAhead extends FilterItemSingleSelect {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      selected: []
+    };
+  }
+  
   updateBindings() {
     Promise.all([
       this.props.filters.getState(this.constructor.getName()),
@@ -9,36 +17,36 @@ class FilterItemTypeAhead extends FilterItemSingleSelect {
     .then(([item]) => {
       // update internal state on reset
       if (item.get(this.constructor.getProperty()) === undefined) {
-        this.setState({ selected: 'all' });
+        this.setState({ selected: [] });
       }
     });
   }
   
   handleChange(filterVal) {
-    console.log(filterVal);
     const { filters } = this.props;
     
     filters.getState()
     .then(value => {
       if (filterVal.length === 0) {
         filters.assign(this.constructor.getName(), value.set(this.constructor.getProperty(), undefined));
-        this.setState({ selected: undefined });
+        this.setState({ selected: [] });
       } else {
         filters.assign(this.constructor.getName(), value.set(this.constructor.getProperty(), filterVal[0]._id));
-        this.setState({ selected: filterVal[0]._id });
+        this.setState({ selected: [filterVal[0]] });
       }
     });
   }
   
   render() {
-    const { data } = this.state;
+    const { data, selected } = this.state;
     
     return (
       <Typeahead id="filter-items"
                  onChange={this.handleChange}
                  options={data === undefined ? [] : data}
                  clearButton={true}
-                 placeholder={"Make a selection"}
+                 placeholder={'Make a selection'}
+                 selected={selected}
                  multiple={false}
       />
     );
