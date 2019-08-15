a5b1d7fdec5e00c49d880a521773739d5768e2cb
Mihai Postelnicu <mpostelnicu@gmail.com>
2019-08-15 16:04:07 +0300

    OCMAKU-256 Corruption Dashboard - Charts - (i180) Multiple Direct Awards

--- web/src/main/java/org/devgateway/ocds/web/flags/release/ReleaseFlagI180Processor.java
+++ web/src/main/java/org/devgateway/ocds/web/flags/release/ReleaseFlagI180Processor.java
@@ -1,13 +1,14 @@
 package org.devgateway.ocds.web.flags.release;
 
 
-import java.util.Arrays;
-import java.util.Collections;
-import javax.annotation.PostConstruct;
 import org.devgateway.ocds.persistence.mongo.FlaggedRelease;
 import org.devgateway.ocds.persistence.mongo.flags.Flag;
 import org.devgateway.ocds.persistence.mongo.flags.preconditions.FlaggedReleasePredicates;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author mpostelnicu
@@ -36,8 +37,9 @@ public class ReleaseFlagI180Processor extends ReleaseFlagI077Processor {
     protected void setPredicates() {
         preconditionsPredicates = Collections.synchronizedList(
                 Arrays.asList(FlaggedReleasePredicates.ACTIVE_AWARD_WITH_DATE,
-                        FlaggedReleasePredicates.TENDER_PROCURING_ENTITY,
-                        FlaggedReleasePredicates.LIMITED_PROCUREMENT_METHOD));
+                        FlaggedReleasePredicates.BUYER,
+                        FlaggedReleasePredicates.DIRECT_PROCUREMENT_METHOD
+                ));
 
         reInitialize();
     }
