8dfd54a76e6ad2d3c24a7c3d7624abc91046ee4a
Mihai Postelnicu <mpostelnicu@gmail.com>
2019-11-21 15:52:07 +0200

    OCMAKU-413 Some tenders not loading on the tender details page on the public portal

--- ui/oce/makueni/tenders/single/Tender.jsx
+++ ui/oce/makueni/tenders/single/Tender.jsx
@@ -4,28 +4,28 @@ import FeedbackPage from '../../FeedbackPage';
 class Tender extends FeedbackPage {
   getFeedbackSubject() {
     const { data, department, fiscalYear } = this.props;
-    
+
     let metadata;
     if (data !== undefined) {
       const tender = data[0];
-      
+
       metadata = ' - ' + tender.tenderTitle
         + ' - ' + department.label
         + ' - ' + fiscalYear.name;
     }
     return escape('Tender' + metadata);
   }
-  
+
   render() {
     const { data } = this.props;
     const { currencyFormatter, formatDate } = this.props.styling.tables;
-    
+
     if (data === undefined) {
       return null;
     }
-    
+
     const tender = data[0];
-    
+
     return (<div>
       <div className="row padding-top-10">
         <div className="col-md-6">
@@ -37,7 +37,7 @@ class Tender extends FeedbackPage {
           <div className="item-value">{tender.tenderNumber}</div>
         </div>
       </div>
-      
+
       <div className="row padding-top-10">
         <div className="col-md-4">
           <div className="item-label">Invitation to Tender Date</div>
@@ -52,14 +52,14 @@ class Tender extends FeedbackPage {
           <div className="item-value">{tender.procurementMethod.label}</div>
         </div>
       </div>
-      
+
       <div className="row padding-top-10">
         <div className="col-md-12">
           <div className="item-label">Tender Objective</div>
           <div className="item-value">{tender.objective}</div>
         </div>
       </div>
-      
+
       <div className="row padding-top-10">
         <div className="col-md-4">
           <div className="item-label">Tender Issued By</div>
@@ -71,10 +71,11 @@ class Tender extends FeedbackPage {
         </div>
         <div className="col-md-4">
           <div className="item-label">Procuring Entity Email</div>
-          <div className="item-value">{tender.issuedBy.emailAddress}</div>
+          <div className="item-value download">
+            <a href={"mailto:"+tender.issuedBy.emailAddress}>{tender.issuedBy.emailAddress}</a></div>
         </div>
       </div>
-      
+
       <div className="row padding-top-10">
         <div className="col-md-6">
           <div className="item-label">Tender Value</div>
@@ -89,7 +90,7 @@ class Tender extends FeedbackPage {
           }
         </div>
       </div>
-      
+
       {
         tender.tenderItems !== undefined
           ? <div>
@@ -98,7 +99,7 @@ class Tender extends FeedbackPage {
                 ({tender.tenderItems.length})
               </div>
             </div>
-            
+
             {
               tender.tenderItems.map(tenderItem => <div key={tenderItem._id} className="box">
                 <div className="row">
@@ -137,12 +138,12 @@ class Tender extends FeedbackPage {
           </div>
           : null
       }
-      
+
       <div className="row padding-top-10">
         <div className="col-md-6">
           <div className="item-label">Download Tender</div>
           {
-            tender.formDocs.map(doc => <div key={doc._id}>
+            tender.formDocs && tender.formDocs.map(doc => <div key={doc._id}>
               <OverlayTrigger
                 placement="bottom"
                 overlay={
@@ -150,7 +151,7 @@ class Tender extends FeedbackPage {
                     Click to download the file
                   </Tooltip>
                 }>
-                
+
                 <a className="item-value download" href={doc.url} target="_blank">
                   <i className="glyphicon glyphicon-download"/>
                   <span>{doc.name}</span>
@@ -161,10 +162,15 @@ class Tender extends FeedbackPage {
         </div>
         <div className="col-md-6">
           <div className="item-label">Tender Link</div>
-          <div className="item-value">{tender.tenderLink}</div>
+          <div className="item-value">
+            <a className="item-value download" href={tender.tenderLink} target="_blank">
+              {tender.tenderLink}
+            </a>
+
+          </div>
         </div>
       </div>
-      
+
       {this.getFeedbackMessage()}
     </div>);
   }
