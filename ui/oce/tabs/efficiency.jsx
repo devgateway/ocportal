6660102f28fa91c755c799d94997a65ebe4f586a
mpostelnicu <mpostelnicu@gmail.com>
2019-12-03 12:01:30 +0200

    OCMAKU-436 Remove cancelled tender amount graph

--- ui/oce/tabs/efficiency.jsx
+++ ui/oce/tabs/efficiency.jsx
@@ -1,6 +1,5 @@
 import Tab from './index';
 import BidPeriod from '../visualizations/charts/bid-period';
-import Cancelled from '../visualizations/charts/cancelled';
 import NrCancelled from '../visualizations/charts/nr-cancelled';
 import TotalAmountsByItem from '../visualizations/charts/total-amounts-by-item';
 import BidsByItem from '../visualizations/charts/bids-by-item';
@@ -13,6 +12,6 @@ class Efficiency extends Tab {
 }
 
 Efficiency.icon = 'efficiency';
-Efficiency.visualizations = [BidPeriod, TotalAmountsByItem, BidsByItem, Cancelled, NrCancelled, PercentWithTenders];
+Efficiency.visualizations = [BidPeriod, TotalAmountsByItem, BidsByItem, NrCancelled, PercentWithTenders];
 
 export default Efficiency;
