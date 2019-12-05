ad3c7e4d1d72878479e68eeff04dab59de625196
mpostelnicu <mpostelnicu@gmail.com>
2019-12-05 21:08:23 +0200

    OCMAKU-450 reverted dashboard url change

--- web/src/main/java/org/devgateway/toolkit/web/spring/WebSecurityConfig.java
+++ web/src/main/java/org/devgateway/toolkit/web/spring/WebSecurityConfig.java
@@ -92,7 +92,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(final WebSecurity web) throws Exception {
         web.httpFirewall(allowUrlEncodedSlashHttpFirewall()).ignoring().
                 antMatchers("/", "/v2/api-docs/**", "/swagger-ui.html**", "/webjars/**", "/images/**",
-                        "/configuration/**", "/swagger-resources/**", "/", "/languages/**",
+                        "/configuration/**", "/swagger-resources/**", "/dashboard", "/languages/**",
                         "/publication-policy.txt",
                         "/isAuthenticated",
                         "/wicket/resource/**/*.ttf", "/wicket/resource/**/*.woff", "/corruption-risk",
