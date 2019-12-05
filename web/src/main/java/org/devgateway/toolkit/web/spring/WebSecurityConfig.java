051cbf70aefae5d394936f3fa37ea050aab9f9cf
Mihai Postelnicu <mpostelnicu@gmail.com>
2019-12-05 18:43:21 +0200

    OCMAKU-450 Change https://opencontracting.makueni.go.ke/ default page to the public portal

--- web/src/main/java/org/devgateway/toolkit/web/spring/WebSecurityConfig.java
+++ web/src/main/java/org/devgateway/toolkit/web/spring/WebSecurityConfig.java
@@ -91,14 +91,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(final WebSecurity web) throws Exception {
         web.httpFirewall(allowUrlEncodedSlashHttpFirewall()).ignoring().
-                antMatchers("/", "/home", "/v2/api-docs/**", "/swagger-ui.html**", "/webjars/**", "/images/**",
-                        "/configuration/**", "/swagger-resources/**", "/dashboard", "/languages/**",
+                antMatchers("/", "/v2/api-docs/**", "/swagger-ui.html**", "/webjars/**", "/images/**",
+                        "/configuration/**", "/swagger-resources/**", "/", "/languages/**",
                         "/publication-policy.txt",
                         "/isAuthenticated",
-                "/wicket/resource/**/*.ttf", "/wicket/resource/**/*.woff", "/corruption-risk",
-                SecurityUtil.getDisabledApiSecurity(adminSettingsRepository) ? "/api/**" : "/",
-                "/wicket/resource/**/*.woff2", "/wicket/resource/**/*.css.map"
-        ).antMatchers(allowedApiEndpoints);
+                        "/wicket/resource/**/*.ttf", "/wicket/resource/**/*.woff", "/corruption-risk",
+                        SecurityUtil.getDisabledApiSecurity(adminSettingsRepository) ? "/api/**" : "/",
+                        "/wicket/resource/**/*.woff2", "/wicket/resource/**/*.css.map"
+                ).antMatchers(allowedApiEndpoints);
 
     }
 
