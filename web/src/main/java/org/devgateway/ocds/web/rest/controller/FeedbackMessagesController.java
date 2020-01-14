f7d38d90da1123613a690e1e9228ff5045b64c97
mpostelnicu <mpostelnicu@gmail.com>
2020-01-14 08:44:24 +0200

    OCMAKU-447

--- web/src/main/java/org/devgateway/ocds/web/rest/controller/FeedbackMessagesController.java
+++ web/src/main/java/org/devgateway/ocds/web/rest/controller/FeedbackMessagesController.java
@@ -1,10 +1,14 @@
 package org.devgateway.ocds.web.rest.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.devgateway.toolkit.persistence.dao.feedback.FeedbackMessage;
 import org.devgateway.toolkit.persistence.dao.feedback.ReplyableFeedbackMessage;
 import org.devgateway.toolkit.persistence.service.feedback.ReplyableFeedbackMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by mpostelnicu
@@ -33,4 +39,72 @@ public class FeedbackMessagesController {
         return feedbackMessageService.findByUrlAndVisibleTrue(page);
     }
 
+    public class FeedbackMessageSubmitWrapper implements Serializable {
+
+        private String name;
+        private String email;
+        private String comment;
+        private Long replyFor;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getComment() {
+            return comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public Long getReplyFor() {
+            return replyFor;
+        }
+
+        public void setReplyFor(Long replyFor) {
+            this.replyFor = replyFor;
+        }
+    }
+
+    @PostMapping(value = "/api/postFeedback",
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> postFeedback(@RequestBody FeedbackMessageSubmitWrapper message) {
+        FeedbackMessage fm;
+        if (message.getReplyFor() != null) {
+            fm = new FeedbackMessage();
+        } else {
+            fm = new ReplyableFeedbackMessage();
+        }
+
+        fm.setEmail(message.getEmail());
+        fm.setName(message.getName());
+        fm.setComment(message.getComment());
+
+        if (message.getReplyFor() != null) {
+            Optional<ReplyableFeedbackMessage> saveable
+                    = feedbackMessageService.findById(message.getReplyFor());
+            if (saveable.isPresent()) {
+                saveable.get().getReplies().add(fm);
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            feedbackMessageService.save((ReplyableFeedbackMessage) fm);
+            return ResponseEntity.ok().build();
+        }
+    }
 }
