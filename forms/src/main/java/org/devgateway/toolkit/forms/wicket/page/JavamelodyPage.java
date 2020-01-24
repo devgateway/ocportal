dd35fbb0e1674188f539ad80243003f72661c30b
Mihai Postelnicu <mpostelnicu@gmail.com>
2020-01-24 16:51:55 +0200

    OCMAKU-518 dgtkit merge issues

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/JavamelodyPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/JavamelodyPage.java
@@ -1,14 +1,13 @@
 package org.devgateway.toolkit.forms.wicket.page;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.pages.RedirectPage;
-import org.apache.wicket.request.flow.RedirectToUrlException;
-import org.devgateway.toolkit.forms.security.SecurityConstants;
+import org.devgateway.toolkit.web.security.SecurityConstants;
+
 
 /**
  * @author mpostelnicu
- *
+ * <p>
  * Simple class the redirect the user to javamelody UI URL (which is an external Wicket URL)
  */
 @AuthorizeInstantiation(SecurityConstants.Roles.ROLE_ADMIN)
