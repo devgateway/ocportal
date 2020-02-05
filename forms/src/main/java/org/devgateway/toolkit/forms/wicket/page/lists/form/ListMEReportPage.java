abb7d3f80254b372ca86075bb21b9e594e69d18a
mpostelnicu <mpostelnicu@gmail.com>
2020-02-05 12:28:44 +0200

    OCMAKU-521 me status column filter

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/lists/form/ListMEReportPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/lists/form/ListMEReportPage.java
@@ -1,10 +1,15 @@
 package org.devgateway.toolkit.forms.wicket.page.lists.form;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.devgateway.toolkit.forms.wicket.components.table.SelectFilteredBootstrapPropertyColumn;
 import org.devgateway.toolkit.forms.wicket.page.edit.form.EditMEReportPage;
 import org.devgateway.toolkit.persistence.dao.form.MEReport;
+import org.devgateway.toolkit.persistence.service.category.MEStatusService;
 import org.devgateway.toolkit.persistence.service.filterstate.JpaFilterState;
 import org.devgateway.toolkit.persistence.service.filterstate.form.MEReportFilterState;
 import org.devgateway.toolkit.persistence.service.form.MEReportService;
@@ -21,6 +26,9 @@ public class ListMEReportPage extends ListAbstractTenderProcessMakueniEntity<MER
     @SpringBean
     protected MEReportService service;
 
+    @SpringBean
+    protected MEStatusService meStatusService;
+
     public ListMEReportPage(final PageParameters parameters) {
         super(parameters);
         this.jpaService = service;
@@ -31,8 +39,19 @@ public class ListMEReportPage extends ListAbstractTenderProcessMakueniEntity<MER
     protected void onInitialize() {
         addTenderTitleColumn();
         addFileDownloadColumn();
+        addMEStatusColumn();
 
         super.onInitialize();
+    }
+
+    protected void addMEStatusColumn() {
+        columns.add(new SelectFilteredBootstrapPropertyColumn<>(
+                new Model<>(
+                        (new StringResourceModel("meStatus", ListMEReportPage.this)).getString()),
+                "meStatus",
+                "meStatus",
+                LoadableDetachableModel.of(() -> meStatusService.findAll()), dataTable, false
+        ));
     }
 
 
--- persistence/src/main/java/org/devgateway/toolkit/persistence/service/filterstate/form/MEReportFilterState.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/service/filterstate/form/MEReportFilterState.java
@@ -1,7 +1,38 @@
 package org.devgateway.toolkit.persistence.service.filterstate.form;
 
+import org.devgateway.toolkit.persistence.dao.categories.MEStatus;
 import org.devgateway.toolkit.persistence.dao.form.MEReport;
+import org.devgateway.toolkit.persistence.dao.form.MEReport_;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MEReportFilterState extends AbstractImplTenderProcessFilterState<MEReport> {
 
+    private MEStatus meStatus;
+
+
+    @Override
+    public Specification<MEReport> getSpecification() {
+        return (root, query, cb) -> {
+            final List<Predicate> predicates = new ArrayList<>();
+
+            if (meStatus != null) {
+                predicates.add(cb.equal(root.get(MEReport_.meStatus), meStatus));
+            }
+
+            predicates.add(super.getSpecification().toPredicate(root, query, cb));
+            return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+        };
+    }
+
+    public MEStatus getMeStatus() {
+        return meStatus;
+    }
+
+    public void setMeStatus(MEStatus meStatus) {
+        this.meStatus = meStatus;
+    }
 }
