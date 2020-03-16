991aee7dd7b6825a448f1efa1e41ef138cfe621c
mpostelnicu <mpostelnicu@gmail.com>
2020-03-16 17:59:58 +0200

    OCMAKU-576

--- ui/oce/visualizations/charts/pie-chart.jsx
+++ ui/oce/visualizations/charts/pie-chart.jsx
@@ -14,17 +14,28 @@ class PieChart extends backendYearFilterable(Chart) {
     const labels = data.map(pluckImm(this.constructor.LABEL_FIELD)).toJS();
     const values = data.map(pluckImm(this.constructor.VALUE_FIELD)).toJS();
     const text = [];
+    const hovertext = [];
+    // const ultimateColors = [
+    //   ['rgb(56, 75, 126)', 'rgb(18, 36, 37)', 'rgb(34, 53, 101)', 'rgb(36, 55, 57)', 'rgb(6, 4, 4)'],
+    // ];
+
     for (let i = 0; i < values.length; i += 1) {
       text.push(this.props.styling.charts.hoverFormatter(values[i]));
+      hovertext.push(this.props.styling.charts.hoverFormatter(values[i])+' '+labels[i]);
     }
     return [{
       values: values,
       labels: labels,
       customdata: values,
       text: text,
+      hovertext: hovertext,
       textposition: 'inside',
       textinfo: 'text',
       hoverinfo: 'text+percent',
+      // marker: {
+      //   colors: ultimateColors[0]
+      // },
+      hovertemplate: this.hoverTemplate(),
       type: 'pie'
     }];
   }
