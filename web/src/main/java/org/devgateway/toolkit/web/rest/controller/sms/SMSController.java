273326c6da0bd637965629722fae83569fb12794
mpostelnicu <mpostelnicu@gmail.com>
2020-03-17 21:05:49 +0200

    OCMAKU-564 added logger info

--- web/src/main/java/org/devgateway/toolkit/web/rest/controller/sms/SMSController.java
+++ web/src/main/java/org/devgateway/toolkit/web/rest/controller/sms/SMSController.java
@@ -1,5 +1,7 @@
 package org.devgateway.toolkit.web.rest.controller.sms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +13,14 @@ import java.util.Map;
 @RestController
 public class SMSController {
 
+    private static final Logger logger = LoggerFactory.getLogger(SMSController.class);
+
+
     @RequestMapping(value = "/api/smsGateway",
             method = {RequestMethod.POST, RequestMethod.GET},
             produces = "application/json")
     public String smsGateway(@RequestParam(value = "key") String key, @RequestBody Map<String, Object> payload) {
+        logger.info("Sms Gateway Received for key=" + key + " payload=" + payload);
         return "Sms Gateway Received for key=" + key + " payload=" + payload;
     }
 }
