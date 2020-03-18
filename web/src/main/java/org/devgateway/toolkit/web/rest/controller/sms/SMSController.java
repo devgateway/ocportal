ab2e6518010919c1d79c5197a487f5b1a0324845
mpostelnicu <mpostelnicu@gmail.com>
2020-03-18 11:43:16 +0200

    OCMAKU-564 added key in the url, because infobip eats it

--- web/src/main/java/org/devgateway/toolkit/web/rest/controller/sms/SMSController.java
+++ web/src/main/java/org/devgateway/toolkit/web/rest/controller/sms/SMSController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -16,12 +15,10 @@ public class SMSController {
     private static final Logger logger = LoggerFactory.getLogger(SMSController.class);
 
 
-    @RequestMapping(value = "/api/smsGateway",
-            method = {RequestMethod.POST, RequestMethod.GET},
-            produces = "application/json")
-    public String smsGateway(@RequestParam(value = "key", required = false)
-                                     String key, @RequestBody Map<String, Object> payload) {
-        String log = "Sms Gateway Received for key=" + (key == null ? "[null]" : key) + " payload=" + payload;
+    @RequestMapping(value = "/api/smsGateway/yz7jAZSR2d32kpElmrDopbd4mWAjF7IA",
+            method = {RequestMethod.POST, RequestMethod.GET}, produces = "application/json")
+    public String smsGateway(@RequestBody Map<String, Object> payload) {
+        String log = "Sms Gateway Received payload=" + payload;
         logger.info(log);
         return log;
     }
