bad7b4471c513d877c1e6cf437b514b29a5d68cf
mpostelnicu <mpostelnicu@gmail.com>
2020-04-07 14:48:32 +0300

    OCMAKU-563 improved sms gateway security

--- web/src/main/java/org/devgateway/toolkit/web/rest/controller/sms/SMSController.java
+++ web/src/main/java/org/devgateway/toolkit/web/rest/controller/sms/SMSController.java
@@ -5,9 +5,13 @@ import org.devgateway.toolkit.persistence.service.sms.SMSMessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,17 +20,25 @@ public class SMSController {
     @Autowired
     private SMSMessageService messageService;
 
+    @Value("${smsgateway.key}")
+    private String smsGatewayKey;
+
     private static final Logger logger = LoggerFactory.getLogger(SMSController.class);
 
-    @RequestMapping(value = "/api/smsGateway/yz7jAZSR2d32kpElmrDopbd4mWAjF7IA",
+    @RequestMapping(value = "/api/smsGateway/{key}",
             method = {RequestMethod.POST, RequestMethod.GET}, produces = "application/json")
-    public String smsGateway(@RequestBody SMSMessageWrapper body) {
+    @ResponseBody
+    public ResponseEntity<String> smsGateway(@RequestBody SMSMessageWrapper body, @PathVariable String key) {
         String log = "Sms Gateway Received " + body.getMessageCount() + " messages.";
+        if (!smsGatewayKey.equals(key)) {
+            logger.error("Invalid smsGatewayKey provided: " + key);
+            return ResponseEntity.badRequest().build();
+        }
         body.getResults().forEach(m -> {
             messageService.save(m);
             messageService.processAndPersistSMSResult(m);
         });
         logger.info(log.toString());
-        return log;
+        return ResponseEntity.ok(log);
     }
 }
