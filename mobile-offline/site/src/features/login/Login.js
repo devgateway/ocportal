6fa815d26d4606c382060448b098f767a5502371
mpostelnicu <mpostelnicu@gmail.com>
2020-08-19 20:39:50 +0300

    some cleanup plus login spinner

--- mobile-offline/site/src/features/login/Login.js
+++ mobile-offline/site/src/features/login/Login.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
+import {Spinner, Form, FormGroup, Label, Input, Button} from 'reactstrap';
 import {performLogin, selectLogin} from './loginSlice';
 
 export function Login() {
@@ -24,7 +24,7 @@ export function Login() {
         return (
             <div className="row">
                 <div className="col-8 offset-2">
-                    <h2>User {login.username} authenticated</h2>
+                    <h2>User {login.user.token} authenticated</h2>
                 </div>
             </div>
         );
@@ -38,17 +38,18 @@ export function Login() {
                             <FormGroup>
                                 <Label>Username</Label>
                                 <Input type="text" name="username" placeholder="username"
-                                       value={userPass.username}
+                                       value={userPass.username} readOnly={login.loading}
                                        onChange={updateField} required/>
                             </FormGroup>
                             <FormGroup>
                                 <Label for="examplePassword">Password</Label>
                                 <Input type="password" name="password" placeholder="********"
-                                       value={userPass.password}
+                                       value={userPass.password} readOnly={login.loading}
                                        onChange={updateField} required/>
                             </FormGroup>
-                            <Button color="primary">Submit</Button>
+                            <Button color="primary" disabled={login.loading}>Submit</Button>
                         </Form>
+                        {login.loading ? <Spinner style={{width: '3rem', height: '3rem'}}/> : ""}
                     </div>
                 </div>
             </div>
--- mobile-offline/site/src/features/login/loginSlice.js
+++ mobile-offline/site/src/features/login/loginSlice.js
@@ -1,63 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit';
-import {isAuthenticated, loginUser} from "../../api/Api";
+import {createSlice} from '@reduxjs/toolkit';
+import {loginUser} from "../../api/Api";
 
 export const loginSlice = createSlice({
-  name: 'login',
-  initialState: {
-    authenticated: false,
-    user: {}
-  },
-  reducers: {
-    loginInvoked: (state, action) => {
-      console.log("Login was invoked for"+ JSON.stringify(action.payload));
+    name: 'login',
+    initialState: {
+        authenticated: false,
+        user: {},
+        loading: false
     },
-    loginFailure: (state, action) => {
-        console.log("Login failed: "+ JSON.stringify(action.payload));
-      state.authenticated = false;
+    reducers: {
+        loginInvoked: (state, action) => {
+            state.loading = true;
+            console.log("Login was invoked for " + JSON.stringify(action.payload));
+        },
+        loginFailure: (state, action) => {
+            console.log("Login failed: " + JSON.stringify(action.payload));
+            state.authenticated = false;
+            state.loading = false;
+        },
+        loginSuccess: (state, action) => {
+            console.log("Login success: " + JSON.stringify(action.payload));
+            state.loading = false;
+            state.user = action.payload;
+            state.authenticated = true;
+        },
     },
-    loginSuccess: (state, action) => {
-        console.log("Login success: "+ JSON.stringify(action.payload));
-      state.user = action.payload;
-      state.authenticated = true;
-    },
-  },
 });
 
-export const { loginInvoked, loginFailure, loginSuccess } = loginSlice.actions;
+export const {loginInvoked, loginFailure, loginSuccess} = loginSlice.actions;
 
 /**
  * Login a user using (username, password) tuple.
  */
 export const performLogin = userPass => dispatch => {
-  dispatch(loginInvoked(userPass));
-
-  //this is in case we use form content disposition, not json:
-  //const data = new FormData();
-  // data.append('username', userPass.username);
-  // data.append('password', userPass.password);
-
-  loginUser(userPass)
-      .then(resolve => {
-              const {data} = resolve;
-              // check if the user is authenticated and save it's details
-              if (data.token) {
-                dispatch(loginSuccess(data));
-                console.log("User authenticated");
-              } else {
-                dispatch(loginFailure("Username or password is incorrect!"));
-              }
+    dispatch(loginInvoked(userPass));
+    setTimeout(() => {
+        loginUser(userPass)
+            .then(resolve => {
+                const {data} = resolve;
+                // check if the user is authenticated and save it's details
+                if (data.token) {
+                    dispatch(loginSuccess(data));
+                    console.log("User authenticated");
+                } else {
+                    dispatch(loginFailure("Username or password is incorrect!"));
+                }
             })
-      .catch(reject => {
-        console.log(reject);
-        dispatch(loginFailure("Network Error! Ensure Internet is up and retry!"));
-      });
+            .catch(reject => {
+                console.log(reject);
+                dispatch(loginFailure("Network Error! Ensure Internet is up and retry!"));
+            });
+    }, 3000); //simulate network delay
 };
 
-
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectLogin = state => state.login;
 
 export default loginSlice.reducer;
