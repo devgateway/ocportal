ed7c49816bd1b03401722a244da2c4bf2ed7ee63
mpostelnicu <mpostelnicu@gmail.com>
2020-08-21 09:21:48 +0300

    change of url to staging

--- mobile-offline/site/src/api/Api.js
+++ mobile-offline/site/src/api/Api.js
@@ -1,6 +1,6 @@
 import {httpPost} from "./Ajax.js";
 
-export const API_ROOT_URL = (document.location.hostname === "localhost") ? "http://localhost:8090" : "";
+export const API_ROOT_URL = (document.location.hostname === "localhost") ? "https://makuenioc.dgstg.org" : "";
 
 const API_LOGIN = API_ROOT_URL + "/api/login";
 
