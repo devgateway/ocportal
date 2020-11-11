03bd450cce0963189c0e9baeb6b4f1e15c59a053
mpostelnicu <mpostelnicu@gmail.com>
2020-11-11 12:04:58 +0200

    OCMAKU-770 contract supplier selector works only if no non draft implementation forms exist

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditContractPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditContractPage.java
@@ -137,7 +137,7 @@ public class EditContractPage extends EditAbstractTenderReqMakueniEntityPage<Con
                 new GenericChoiceProvider<>(getAcceptedSupplier(editForm.getModelObject().getTenderProcess()))
         );
         awardeeSelector.required();
-        awardeeSelector.setEnabled(editForm.getModelObject().isNew());
+        awardeeSelector.setEnabled(!editForm.getModelObject().getTenderProcess().hasNonDraftImplForms());
         awardeeSelector.getField().add(new AwardeeAjaxComponentUpdatingBehavior("change"));
         editForm.add(awardeeSelector);
 
