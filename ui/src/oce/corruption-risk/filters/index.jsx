669c4ffcb1a73f7c78356c5fe07eb99be2bdbbd0
Octavian Ciubotaru <octavian@ciubotaru.net>
2020-12-11 14:45:24 +0200

    OCMAKU-800: Converted to functional component

--- ui/src/oce/corruption-risk/filters/index.jsx
+++ ui/src/oce/corruption-risk/filters/index.jsx
@@ -3,47 +3,47 @@ import Organizations from './organizations';
 import ProcurementMethodBox from './procurement-method';
 import ValueAmount from './value-amount';
 import DateBox from './date';
-import translatable from '../../translatable';
+import {tCreator} from '../../translatable';
 import ProcurementMethodRationaleBox from './procurement-method-rationale';
 
-class Filters extends translatable(React.Component) {
-  render() {
-    const { onUpdate, translations, currentBoxIndex, requestNewBox, state, allYears, allMonths, onApply, appliedFilters } = this.props;
-    const { BOXES } = this.constructor;
-    return (
-      <div className="row filters-bar" onMouseDown={e => e.stopPropagation()}>
-        <div className="col-md-12 crd-horizontal-filters">
-        <div className="crd-filter-title">
-        <div className="title">{this.t('filters:hint')}</div>
-        </div>
-          {BOXES.map((Box, index) => {
-            return (
-              <Box
-                key={index}
-                open={currentBoxIndex === index}
-                onClick={e => requestNewBox(currentBoxIndex === index ? null : index)}
-                state={state}
-                onUpdate={(slug, newState) => onUpdate(state.set(slug, newState))}
-                translations={translations}
-                onApply={newState => onApply(newState)}
-                allYears={allYears}
-                allMonths={allMonths}
-                appliedFilters={appliedFilters}
-              />
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
-}
-
-Filters.BOXES = [
+const BOXES = [
   DateBox,
   ValueAmount,
   ProcurementMethodBox,
   ProcurementMethodRationaleBox,
   Organizations
 ];
+
+const Filters = props => {
+  const { onUpdate, translations, currentBoxIndex, requestNewBox, state, allYears, allMonths, onApply, appliedFilters } = props;
+
+  const t = tCreator(props.translations);
+
+  return (
+    <div className="row filters-bar" onMouseDown={e => e.stopPropagation()}>
+      <div className="col-md-12 crd-horizontal-filters">
+        <div className="crd-filter-title">
+          <div className="title">{t('filters:hint')}</div>
+        </div>
+        {BOXES.map((Box, index) => {
+          return (
+            <Box
+              key={index}
+              open={currentBoxIndex === index}
+              onClick={e => requestNewBox(currentBoxIndex === index ? null : index)}
+              state={state}
+              onUpdate={(slug, newState) => onUpdate(state.set(slug, newState))}
+              translations={translations}
+              onApply={newState => onApply(newState)}
+              allYears={allYears}
+              allMonths={allMonths}
+              appliedFilters={appliedFilters}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default Filters;
