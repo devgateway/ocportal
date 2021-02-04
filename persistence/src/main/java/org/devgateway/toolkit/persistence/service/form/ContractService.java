0d9dceaba9452dbaea5eac307d2f631912a37b69
Octavian Ciubotaru <octavian@ciubotaru.net>
2021-02-04 12:35:38 +0200

    OCMAKU-906: Validations related to tender process

--- persistence/src/main/java/org/devgateway/toolkit/persistence/service/form/ContractService.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/service/form/ContractService.java
@@ -6,6 +6,6 @@ import org.devgateway.toolkit.persistence.dao.form.TenderProcess;
 /**
  * @author gmutuhu
  */
-public interface ContractService extends AbstractMakueniEntityService<Contract> {
+public interface ContractService extends AbstractTenderProcessEntityService<Contract> {
     Contract findByTenderProcess(TenderProcess tenderProcess);
 }
