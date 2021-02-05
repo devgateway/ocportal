fa9f3650b5550ae63d53c7976f9e5df265fa7f53
Octavian Ciubotaru <octavian@ciubotaru.net>
2021-02-05 18:11:49 +0200

    OCMAKU-906: Restored the code that kept the caches up to date
    (cherry picked from commit e0d9c2f5a664dc4ea3acc3800278e151c696d307)

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditAdministratorReportPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/form/EditAdministratorReportPage.java
@@ -6,6 +6,7 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.devgateway.toolkit.forms.wicket.components.util.ComponentUtil;
 import org.devgateway.toolkit.forms.wicket.page.edit.roleassignable.TechAdminRoleAssignable;
 import org.devgateway.toolkit.persistence.dao.form.AdministratorReport;
+import org.devgateway.toolkit.persistence.dao.form.TenderProcess;
 import org.devgateway.toolkit.persistence.service.form.AdministratorReportService;
 import org.devgateway.toolkit.persistence.service.form.TenderProcessService;
 import org.devgateway.toolkit.web.security.SecurityConstants;
@@ -49,4 +50,23 @@ public class EditAdministratorReportPage extends EditAbstractImplTenderProcessEn
         ComponentUtil.addYesNoToggle(editForm, "authorizePayment", true);
         ComponentUtil.addDateField(editForm, "approvedDate");
     }
+
+    @Override
+    protected void beforeSaveEntity(final AdministratorReport report) {
+        super.beforeSaveEntity(report);
+
+        final TenderProcess tenderProcess = report.getTenderProcess();
+        tenderProcess.addAdministratorReport(report);
+        tenderProcessService.save(tenderProcess);
+    }
+
+    @Override
+    protected void beforeDeleteEntity(final AdministratorReport report) {
+        super.beforeDeleteEntity(report);
+
+        final TenderProcess tenderProcess = report.getTenderProcess();
+        tenderProcess.removeAdministratorReport(report);
+        tenderProcessService.save(tenderProcess);
+    }
+
 }
