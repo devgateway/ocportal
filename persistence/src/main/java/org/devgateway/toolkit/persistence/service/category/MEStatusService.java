af5173c26e311cd6d696385120320e045b0e1474
Mihai Postelnicu <mpostelnicu@gmail.com>
2021-02-19 09:52:14 +0200

    OCMAKU-959 refactored text searching TextSearchableRepository, some other cleanup

--- persistence/src/main/java/org/devgateway/toolkit/persistence/service/category/MEStatusService.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/service/category/MEStatusService.java
@@ -4,13 +4,11 @@
 package org.devgateway.toolkit.persistence.service.category;
 
 import org.devgateway.toolkit.persistence.dao.categories.MEStatus;
-import org.devgateway.toolkit.persistence.service.BaseJpaService;
-import org.devgateway.toolkit.persistence.service.TextSearchableService;
 
 /**
  * @author mpostelnicu
  *
  */
-public interface MEStatusService extends BaseJpaService<MEStatus>, TextSearchableService<MEStatus> {
+public interface MEStatusService extends CategoryService<MEStatus> {
 
 }
