5c5265878d742d79a0ef0445ab5a5a8ca56fd5dd
Mihai Postelnicu <mpostelnicu@gmail.com>
2021-02-24 00:17:02 +0200

    OCMAKU-955 fixes bugs 1 to 5

--- persistence/src/main/java/org/devgateway/toolkit/persistence/service/prequalification/PrequalificationSchemaService.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/service/prequalification/PrequalificationSchemaService.java
@@ -1,8 +1,11 @@
 package org.devgateway.toolkit.persistence.service.prequalification;
 
+import org.devgateway.toolkit.persistence.dao.DBConstants;
 import org.devgateway.toolkit.persistence.dao.prequalification.PrequalificationSchema;
+import org.devgateway.toolkit.persistence.dao.prequalification.PrequalificationSchema_;
 import org.devgateway.toolkit.persistence.service.BaseJpaService;
 import org.devgateway.toolkit.persistence.service.TextSearchableService;
+import org.springframework.data.jpa.domain.Specification;
 
 public interface PrequalificationSchemaService extends BaseJpaService<PrequalificationSchema>,
         TextSearchableService<PrequalificationSchema> {
@@ -10,4 +13,9 @@ public interface PrequalificationSchemaService extends BaseJpaService<Prequalifi
     long countByNameOrPrefix(PrequalificationSchema schema);
 
     Long saveDuplicateSchema(Long schemaId);
+
+    static Specification<PrequalificationSchema> selectableSpecification() {
+        return (r, cq, cb) ->
+                cb.and(cb.equal(r.get(PrequalificationSchema_.status), DBConstants.Status.SUBMITTED));
+    }
 }
