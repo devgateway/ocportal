d5347c95de125b1d6d41ef7ce49639ebc50981c2
Octavian Ciubotaru <octavian@ciubotaru.net>
2021-02-25 09:42:19 +0200

    OCMAKU-956: Unique prequalified supplier items validator

--- persistence/src/main/java/org/devgateway/toolkit/persistence/dao/prequalification/PrequalifiedSupplier.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/dao/prequalification/PrequalifiedSupplier.java
@@ -25,6 +25,7 @@ import java.util.List;
 @Audited
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @UniquePrequalifiedSupplier(groups = HighLevel.class)
+@UniquePrequalifiedSupplierItem
 public class PrequalifiedSupplier extends AbstractAuditableEntity {
 
     @NotNull
