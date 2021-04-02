5a09b01bdb77325e7875a23435a0f52d947ba712
Mihai Postelnicu <mpostelnicu@gmail.com>
2021-04-02 11:32:43 +0300

    OCMAKU-999

--- persistence/src/main/java/org/devgateway/toolkit/persistence/dao/DBConstants.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/dao/DBConstants.java
@@ -24,7 +24,7 @@ public final class DBConstants {
     public static final String DATE_FORMAT = "dd/MM/YYYY";
 
 
-    public static final String FROM_EMAIL = "noreply@dgstg.org";
+    public static final String FROM_EMAIL = "info@elgeyomarakwet.go.ke";
 
     public static final String INSTANCE_NAME = "Elgeyo Marakwet";
 
