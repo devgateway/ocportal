0d89ead11a4192f9efc82f20e27e107e32c6d9c2
brianbrix <mokandubrian@gmail.com>
2025-02-25 11:25:24 +0300

    OCE-546: Upgrade react and other front libraries

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,5 +2,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'leaflet/dist/leaflet.css';
 import './i18n';
 
-document.title = `${process.env.ORG_NAME} Open Contracting Portal` || 'Makueni Open Contracting Portal';
+document.title = `${process.env.REACT_APP_TITLE}` || 'Makueni Open Contracting Portal';
 require('./oce-standalone/index');
